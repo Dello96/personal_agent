b7582770fda9e304981bbfded02bfcbfccd6b449
Dello96 <enadios03@gmail.com>
2025-11-18 04:25:15 +0900

    delete:unnecessary_html_file

---
+++ src/app/frontend/components/shared/Header 2.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header>
+            
+            <Link href="/">
+            <h1>Home</h1>
+            </Link>
+        </header>
+    );
+}
