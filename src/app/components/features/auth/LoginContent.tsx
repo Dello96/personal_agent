19c468ee9d2f44ef33b50efb637b78c57f20eb6e
Dello96 <enadios03@gmail.com>
2025-11-21 18:05:25 +0900

    update:loginUI

--- src/app/components/features/auth/LoginContent.tsx
+++ src/app/components/features/auth/LoginContent.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import Image from "next/image";
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 
 export default function LoginContent() {
+  const kakaoLoginImageUrl = "/images/kakao_login_medium_narrow.png";
+  const googleLoginImageUrl = "/images/web_light_sq_ctn@1x.png";
   const handleGoogleLogin = () => {
     // 백엔드 서버의 Google OAuth 엔드포인트로 이동
     window.location.href = `${API_URL}/login`;
@@ -14,18 +18,30 @@ export default function LoginContent() {
   };
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 justify-self-center">
       <button
+        className="relative w-full max-w-sm rounded-lg overflow-hidden hover:opacity-90 transition-opacity shadow-md hover:shadow-lg"
         onClick={handleGoogleLogin}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
-        Google Login
+        <Image
+          src={googleLoginImageUrl}
+          alt="google 로그인"
+          width={183}
+          height={45}
+          className="w-183 h-45"
+        />
       </button>
       <button
         onClick={handleKakaoLogin}
-        className="px-4 py-2 bg-yellow-400 text-black rounded hover:bg-yellow-500"
+        className="relative w-full max-w-sm rounded-lg overflow-hidden hover:opacity-90 transition-opacity shadow-md hover:shadow-lg"
       >
-        Kakao Login
+        <Image
+          src={kakaoLoginImageUrl}
+          alt="kakao 로그인"
+          width={183}
+          height={45}
+          className="w-183 h-45"
+        />
       </button>
     </div>
   );
